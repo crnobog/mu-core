fb68aacc3883f1ae38bfbb770e8268a8826bf372
Robert Millar <robert.millar@cantab.net>
2017-03-13 23:35:31 -0700

    Handle duplicate definitions of windows include slimming macros

--- Include/mu-core/mu-core.h
+++ Include/mu-core/mu-core.h
@@ -2,9 +2,15 @@
 
 #ifdef MU_CORE_IMPL
 
+#ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <Windows.h>
 
 #endif
