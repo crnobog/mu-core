70612eafad5c2ed8ef2afabdca1ff1dfa0137610
Robert Millar <robert.millar@cantab.net>
2017-09-09 14:16:08 -0700

    Add Global header, mark PointerRange functions as __forceinline, make mu-core builds inline __forceinline functions in debug builds

--- Include/mu-core/PointerRange.h
+++ Include/mu-core/PointerRange.h
@@ -1,5 +1,6 @@
-#pragma once
+﻿#pragma once
 
+#include "mu-core/Global.h"
 #include "mu-core/RangeIteration.h"
 
 namespace mu {
@@ -19,34 +20,30 @@ namespace mu {
 		template<class U>
 		friend class PointerRange;
 
-		PointerRange()
+		FORCEINLINE PointerRange()
 			: m_start(nullptr)
 			, m_end(nullptr) {}
 
-		PointerRange(T* start, T* end)
+		FORCEINLINE PointerRange(T* start, T* end)
 			: m_start(start)
 			, m_end(end) {}
 
 		template<typename U>
-		PointerRange(PointerRange<U> other) {
+		FORCEINLINE PointerRange(PointerRange<U> other) {
 			m_start = other.m_start;
 			m_end = other.m_end;
 		}
 
-		void Advance() { ++m_start; }
-		void AdvanceBy(size_t num) { m_start += num; }
+		FORCEINLINE void Advance() { ++m_start; }
+		FORCEINLINE void AdvanceBy(size_t num) { m_start += num; }
 
-		bool IsEmpty() const { return m_start >= m_end; }
-		T& Front() { return *m_start; }
-		const T& Front() const { return *m_start; }
-		size_t Size() const { return m_end - m_start; }
+		FORCEINLINE bool IsEmpty() const { return m_start >= m_end; }
+		FORCEINLINE T& Front() { return *m_start; }
+		FORCEINLINE const T& Front() const { return *m_start; }
+		FORCEINLINE size_t Size() const { return m_end - m_start; }
 
-		T& operator[](size_t index) {
-			return m_start[index];
-		}
-		const T& operator[](size_t index) const {
-			return m_start[index];
-		}
+		FORCEINLINE T& operator[](size_t index) { return m_start[index]; }
+		FORCEINLINE const T& operator[](size_t index) const { return m_start[index]; }
 
 		template<typename U>
 		bool operator==(const PointerRange<U>& other) const {
@@ -61,69 +58,48 @@ namespace mu {
 		}
 	};
 
+	// A PointerRange can be used by algorithms with no further conversion
 	template<typename T>
-	auto Range(PointerRange<T> r) { return r; }
+	FORCEINLINE auto Range(PointerRange<T> r) { return r; }
 
 	template<typename T>
 	class Array;
 
 	template<typename T, size_t MAX>
 	class FixedArray;
-	
-	template<typename T>
-	auto Range(T* ptr, size_t num) {
-		return Range(ptr, ptr + num);
-	}
 
 	template<typename T>
-	auto Range(T* start, T* end) {
-		return PointerRange<T>(start, end);
-	}
+	FORCEINLINE auto Range(T* ptr, size_t num) { return Range(ptr, ptr + num); }
+
+	template<typename T>
+	FORCEINLINE auto Range(T* start, T* end) { return PointerRange<T>(start, end); }
 
 	template<typename T, size_t SIZE>
-	auto Range(T(&arr)[SIZE]) {
-		return Range(arr, arr + SIZE);
-	}
+	FORCEINLINE auto Range(T(&arr)[SIZE]) { return Range(arr, arr + SIZE); }
 
 	template<typename T>
-	auto Range(Array<T>& arr) {
-		return Range(arr.Data(), arr.Num());
-	}
+	FORCEINLINE auto Range(Array<T>& arr) { return Range(arr.Data(), arr.Num()); }
 
 	template<typename T>
-	auto Range(const Array<T>& arr) {
-		return Range(arr.Data(), arr.Num());
-	}
+	FORCEINLINE auto Range(const Array<T>& arr) { return Range(arr.Data(), arr.Num()); }
 
 	template<typename T, size_t MAX>
-	auto Range(FixedArray<T, MAX>& arr) {
-		return Range(arr.Data(), arr.Num());
-	}
+	FORCEINLINE auto Range(FixedArray<T, MAX>& arr) { return Range(arr.Data(), arr.Num()); }
 
 	template<typename T, size_t MAX>
-	auto Range(const FixedArray<T, MAX>& arr) {
-		return Range(arr.Data(), arr.Num());
-	}
+	FORCEINLINE auto Range(const FixedArray<T, MAX>& arr) { return Range(arr.Data(), arr.Num()); }
 
 	template<typename T>
-	auto ByteRange(const T& t) {
-		return Range((const u8*)&t, sizeof(T));
-	}
+	FORCEINLINE auto ByteRange(const T& t) { return Range((const u8*)&t, sizeof(T)); }
 
 	template<typename T>
-	auto ByteRange(T* t, size_t num) {
-		return Range((u8*)t, num * sizeof(T));
-	}
+	FORCEINLINE auto ByteRange(T* t, size_t num) { return Range((u8*)t, num * sizeof(T)); }
 
 	template<typename T>
-	auto ByteRange(const T* t, size_t num) {
-		return Range((const u8*)t, num * sizeof(T));
-	}
+	FORCEINLINE auto ByteRange(const T* t, size_t num) { return Range((const u8*)t, num * sizeof(T)); }
 
 	template<typename T>
-	auto ByteRange(T* t) {
-		return Range((u8*)&t, sizeof(T));
-	}
+	FORCEINLINE auto ByteRange(T* t) { return Range((u8*)&t, sizeof(T)); }
 }
 
 #ifdef DOCTEST_LIBRARY_INCLUDED
